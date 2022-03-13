a2db8a162c0887a8a3754a78838ca66789398d7c
hyokoya <hyokoya@gmail.com>
2022-03-13 23:32:40 +0900

    installed black formatter

--- capture_board.py
+++ capture_board.py
@@ -1,78 +1,92 @@
-import numpy as np
 import cv2
+import numpy as np
+
 
 def directs(pt):
-        return -pt[0]-pt[1],-pt[0]+pt[1],pt[0]-pt[1],pt[0]+pt[1]
-    
-def get_argmax_directs(pix_arr,i):
-    return np.argmax(np.array([ directs(pix) for pix in pix_arr ])[:,i])
+    return -pt[0] - pt[1], -pt[0] + pt[1], pt[0] - pt[1], pt[0] + pt[1]
 
-def get_corners(pix_arr):
-    corners = np.array([ pix_arr[get_argmax_directs(pix_arr,i)] for i in range(4) ])
+
+def get_argmax_directs(pix_arr):
+    return np.argmax(np.array([directs(pix) for pix in pix_arr]), axis=0)
+
+
+def get_corners(frame):
+    pix_arr = np.array(np.where(frame > 0)).T
+    corners = np.array([pix_arr[idx] for idx in get_argmax_directs(pix_arr)])
     return corners
 
-def get_cell(output,i, width, height, bin_threshold=150):
-    j, k = i//9, i%9
-    cell = output[height*j:height*(j+1),width*k:width*(k+1)]
-    cell = 255 - (cell - np.min(cell))/(np.max(cell) - np.min(cell)) * 255
+
+def get_cell(output, i, width, height, bin_threshold=150):
+    j, k = i // 9, i % 9
+    cell = output[height * j : height * (j + 1), width * k : width * (k + 1)]
+    cell = 255 - (cell - np.min(cell)) / (np.max(cell) - np.min(cell)) * 255
     ret, cell = cv2.threshold(cell, bin_threshold, 255, cv2.THRESH_BINARY)
-#     cell = cv2.adaptiveThreshold(cell.astype(np.uint8),255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-#             cv2.THRESH_BINARY,11,2)
-#     cell = cv2.morphologyEx(cell, cv2.MORPH_ERODE, (3,3), 1)
-    cell = cv2.morphologyEx(cell, cv2.MORPH_OPEN, (5,5), 1)
-#     cell = cv2.morphologyEx(cell, cv2.MORPH_CLOSE, (5,5), 1)
-    cell = cv2.morphologyEx(cell, cv2.MORPH_DILATE, (3,3), 1)
+    #     cell = cv2.adaptiveThreshold(cell.astype(np.uint8),255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+    #             cv2.THRESH_BINARY,11,2)
+    #     cell = cv2.morphologyEx(cell, cv2.MORPH_ERODE, (3,3), 1)
+    cell = cv2.morphologyEx(cell, cv2.MORPH_OPEN, (5, 5), 1)
+    #     cell = cv2.morphologyEx(cell, cv2.MORPH_CLOSE, (5,5), 1)
+    cell = cv2.morphologyEx(cell, cv2.MORPH_DILATE, (3, 3), 1)
     return cell
 
-def capture_image(path, scale=0.3, width=28, height=28, bin_threshold=150):
-    board_width = width*9
-    board_height = height*9
 
-    image = cv2.imread(path,0)
+def capture_image(path, scale=0.3, width=28, height=28, bin_threshold=150):
+    board_width = width * 9
+    board_height = height * 9
+
+    image = cv2.imread(path, 0)
     image_resized = cv2.resize(image, None, fx=scale, fy=scale)
 
-    # Laplacian transform 
+    # Laplacian transform
     image_lap = cv2.Laplacian(image_resized, cv2.CV_32F, ksize=29)
     lap_med, lap_max = np.median(image_lap.ravel()), np.max(image_lap.ravel())
-    image_lap_norm = (image_lap - lap_med)/(lap_max-lap_med)
-    image_lap_norm[image_lap_norm<0] = 0
-    image_lap_pow = pow(image_lap_norm, 1/2)
-    image_lap_pow = (image_lap_pow*255).astype(np.uint8)
+    image_lap_norm = (image_lap - lap_med) / (lap_max - lap_med)
+    image_lap_norm[image_lap_norm < 0] = 0
+    image_lap_pow = pow(image_lap_norm, 1 / 2)
+    image_lap_pow = (image_lap_pow * 255).astype(np.uint8)
 
     # connected components
     retval, labels, stats, centroids = cv2.connectedComponentsWithStats(
-        (image_lap_pow>50).astype(np.uint8)
+        (image_lap_pow > 50).astype(np.uint8)
     )
     border_labels = list(
-        set(np.unique(labels[0,:])) | \
-        set(np.unique(labels[:,0])) | \
-        set(np.unique(labels[-1,:])) | \
-        set(np.unique(labels[:,-1]))
+        set(np.unique(labels[0, :]))
+        | set(np.unique(labels[:, 0]))
+        | set(np.unique(labels[-1, :]))
+        | set(np.unique(labels[:, -1]))
     )
-    full_labels = [ label for label in range(len(stats)) if label not in border_labels ]
+    full_labels = [label for label in range(len(stats)) if label not in border_labels]
     full_label_image = labels.copy()
     for border in border_labels:
-        full_label_image[full_label_image==border] = 0
+        full_label_image[full_label_image == border] = 0
 
     # detect outer frame
-    candidate_idx = np.argmax(stats[full_labels][:,-1])
+    candidate_idx = np.argmax(stats[full_labels][:, -1])
     candidate = stats[full_labels][candidate_idx]
-    candidate_original_idx = np.array(list(range(len(stats))))[full_labels][candidate_idx]
-    frame = (full_label_image==candidate_original_idx).astype(np.uint8)*255
+    candidate_original_idx = np.array(list(range(len(stats))))[full_labels][
+        candidate_idx
+    ]
+    frame = (full_label_image == candidate_original_idx).astype(np.uint8) * 255
 
     # detect corners of the outer frame
-    pix_arr = np.array(np.where(frame>0)).T
-    corners = np.array(get_corners(pix_arr))
-    
+    corners = np.array(get_corners(frame))
+
     # 射影変換
     ## 変更前の4点
     src = np.float32(corners)
     ## 変換後の4点
-    dst = np.float32([[0,0],[0,board_width],[board_height,0],[board_height,board_width]])
+    dst = np.float32(
+        [[0, 0], [0, board_width], [board_height, 0], [board_height, board_width]]
+    )
     ## 変換行列
-    M = cv2.getPerspectiveTransform(src[:,::-1], dst[:,::-1])
+    M = cv2.getPerspectiveTransform(src[:, ::-1], dst[:, ::-1])
     ## 射影変換・透視変換する
-    output = cv2.warpPerspective(image_resized,M,(board_width, board_height))
+    output = cv2.warpPerspective(image_resized, M, (board_width, board_height))
 
-    cells = np.array([ get_cell(output,i,width,height,bin_threshold=bin_threshold) for i in range(81) ])
+    cells = np.array(
+        [
+            get_cell(output, i, width, height, bin_threshold=bin_threshold)
+            for i in range(81)
+        ]
+    )
     return cells
--- number_place.py
+++ number_place.py
@@ -1,26 +1,27 @@
 import time
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 
-import matplotlib.pyplot as plt
 
-class Cell():
+class Cell:
     def __init__(self, row, col, id, val=0):
         self.row = row
         self.col = col
         self.id = id
-        self.block = (row-1)//3*3 + (col-1)//3 + 1
+        self.block = (row - 1) // 3 * 3 + (col - 1) // 3 + 1
         self.__val = val
-        if self.__val==0:
-            self.__candidates = set(range(1,10))
+        if self.__val == 0:
+            self.__candidates = set(range(1, 10))
         else:
             self.__candidates = set()
-    
+
     @property
     def val(self):
         return self.__val
-    
+
     @val.setter
     def val(self, value):
         self.__val = value
@@ -29,67 +30,81 @@ class Cell():
     @property
     def candidates(self):
         return self.__candidates
-    
-    def remove_from_candidates(self,val):
+
+    def remove_from_candidates(self, val):
         if val in self.__candidates:
             self.__candidates.remove(val)
-        
-    def fill_unique(self,):
-        if self.val==0 and len(self.candidates)==1:
+
+    def fill_unique(
+        self,
+    ):
+        if self.val == 0 and len(self.candidates) == 1:
             self.val = list(self.candidates)[0]
 
-           
-class Board():
+
+class Board:
     def __init__(self, board_init=[]):
         self.__solver = Solver()
         self.reset(board_init)
-    
+
     def reset(self, board_init=None):
         if board_init is not None:
             self.__board_init = board_init
 
-        self.__cell_df = pd.DataFrame(columns=['row','col','block','val'])
+        self.__cell_df = pd.DataFrame(columns=["row", "col", "block", "val"])
         k = 0
-        for i in range(1,10):
-            for j in range(1,10):
-                self.__cell_df.loc[k,:] = [i,j,(i-1)//3*3+(j-1)//3+1,0]
-                k+=1
+        for i in range(1, 10):
+            for j in range(1, 10):
+                self.__cell_df.loc[k, :] = [
+                    i,
+                    j,
+                    (i - 1) // 3 * 3 + (j - 1) // 3 + 1,
+                    0,
+                ]
+                k += 1
 
         for elem in self.__board_init:
-            k = (elem['row']-1)*9 + elem['col']-1
-            self.__cell_df.loc[k,'val'] = elem['val']
+            k = (elem["row"] - 1) * 9 + elem["col"] - 1
+            self.__cell_df.loc[k, "val"] = elem["val"]
 
         self.__solver.reset(self.__board_init)
-    
-    def count_unfilled(self):
-        return sum([ val==0 for val in self.__cell_df.val ])
-        
-    def draw(self,init=False):
-        if init:
-            board_arr = np.zeros((9,9), dtype=int)
-            for elem in self.__board_init:
-                row = elem['row']
-                col = elem['col']
-                val = elem['val']
-                board_arr[row-1,col-1] = val
-        else:
-            board_arr = np.zeros((9,9), dtype=int)
-            for row, col, val in zip(self.__cell_df.row,self.__cell_df.col,self.__cell_df.val):
-                board_arr[row-1,col-1] = val
 
-        plt.figure(figsize=(6,6))
-        ax = sns.heatmap(board_arr, 
-                         cmap='tab10', vmin=0, vmax=9,
-                         square=True, 
-                         annot=True, annot_kws={"size": 24},
-                         linewidths=.5, linecolor="White",
-                         cbar=False,
-                        )
-        ax.axis('off')
-        ax.axhline(y=3, color='w',linewidth=5)
-        ax.axhline(y=6, color='w',linewidth=5)
-        ax.axvline(x=3, color='w',linewidth=5)
-        ax.axvline(x=6, color='w',linewidth=5)
+    def count_unfilled(self):
+        return sum([val == 0 for val in self.__cell_df.val])
+
+    def draw(self, init=False):
+        if init:
+            board_arr = np.zeros((9, 9), dtype=int)
+            for elem in self.__board_init:
+                row = elem["row"]
+                col = elem["col"]
+                val = elem["val"]
+                board_arr[row - 1, col - 1] = val
+        else:
+            board_arr = np.zeros((9, 9), dtype=int)
+            for row, col, val in zip(
+                self.__cell_df.row, self.__cell_df.col, self.__cell_df.val
+            ):
+                board_arr[row - 1, col - 1] = val
+
+        plt.figure(figsize=(6, 6))
+        ax = sns.heatmap(
+            board_arr,
+            cmap="tab10",
+            vmin=0,
+            vmax=9,
+            square=True,
+            annot=True,
+            annot_kws={"size": 24},
+            linewidths=0.5,
+            linecolor="White",
+            cbar=False,
+        )
+        ax.axis("off")
+        ax.axhline(y=3, color="w", linewidth=5)
+        ax.axhline(y=6, color="w", linewidth=5)
+        ax.axvline(x=3, color="w", linewidth=5)
+        ax.axvline(x=6, color="w", linewidth=5)
         plt.tight_layout()
         plt.show()
 
@@ -97,7 +112,9 @@ class Board():
         self.draw(init=True)
 
     def list_candidates(self):
-        return [ cell.candidates if len(cell.candidates)>0 else 0 for cell in self.__cells ]
+        return [
+            cell.candidates if len(cell.candidates) > 0 else 0 for cell in self.__cells
+        ]
 
     def solve(self):
         self.__solver.solve()
@@ -112,48 +129,48 @@ class Board():
         self.__cell_df.val = self.__solver.get_current_cell_values()
 
 
-class Solver():
+class Solver:
     def __init__(self, board_init=[]):
         self.reset(board_init)
 
     def reset(self, board_init):
         self.__cells = []
         k = 0
-        for i in range(1,10):
-            for j in range(1,10):
-                self.__cells.append(Cell(i,j,k,0))
-                k+=1
+        for i in range(1, 10):
+            for j in range(1, 10):
+                self.__cells.append(Cell(i, j, k, 0))
+                k += 1
 
         for elem in board_init:
-            k = (elem['row']-1)*9 + elem['col']-1
-            self.__cells[k].val = elem['val']
+            k = (elem["row"] - 1) * 9 + elem["col"] - 1
+            self.__cells[k].val = elem["val"]
 
     def count_unfilled(self):
-        return sum([ cell.val==0 for cell in self.__cells ])
+        return sum([cell.val == 0 for cell in self.__cells])
 
     def get_current_cell_values(self):
-        return [ cell.val for cell in self.__cells ]
-  
+        return [cell.val for cell in self.__cells]
+
     def check_uniques(self):
-        [ cell.fill_unique() for cell in self.__cells ]
+        [cell.fill_unique() for cell in self.__cells]
 
     def check_all_cols(self):
-        for col in range(1,10):
-            selected_cells = [ cell for cell in self.__cells if cell.col==col ]
-            self.__check_nine(selected_cells)
-            
-    def check_all_rows(self):
-        for row in range(1,10):
-            selected_cells = [ cell for cell in self.__cells if cell.row==row ]
-            self.__check_nine(selected_cells)
-    
-    def check_all_blocks(self):
-        for block in range(1,10):
-            selected_cells = [ cell for cell in self.__cells if cell.block==block ]
+        for col in range(1, 10):
+            selected_cells = [cell for cell in self.__cells if cell.col == col]
             self.__check_nine(selected_cells)
 
-    def __check_nine(self,selected_cells):
-        update_candidates_in_nine(selected_cells)        
+    def check_all_rows(self):
+        for row in range(1, 10):
+            selected_cells = [cell for cell in self.__cells if cell.row == row]
+            self.__check_nine(selected_cells)
+
+    def check_all_blocks(self):
+        for block in range(1, 10):
+            selected_cells = [cell for cell in self.__cells if cell.block == block]
+            self.__check_nine(selected_cells)
+
+    def __check_nine(self, selected_cells):
+        update_candidates_in_nine(selected_cells)
         unique_candidates = find_unique_candidates(selected_cells)
         self.fill_uniques(unique_candidates)
         update_candidates_in_nine(selected_cells)
@@ -161,62 +178,79 @@ class Solver():
         exclusive_pairings(selected_cells, pairing_list)
         update_candidates_in_nine(selected_cells)
 
-    def fill_uniques(self,fill_list):
+    def fill_uniques(self, fill_list):
         for fill in fill_list:
-            self.__cells[fill['index']].val = fill['unique']
+            self.__cells[fill["index"]].val = fill["unique"]
 
     def step(self):
         self.check_all_rows()
         self.check_all_cols()
         self.check_all_blocks()
         self.check_uniques()
-        
-    def solve(self,):
+
+    def solve(
+        self,
+    ):
         t0 = time.time()
         step = 0
-        while self.count_unfilled()>0:
+        while self.count_unfilled() > 0:
             self.step()
-            step+=1
-        print(f'time to solve: {time.time()-t0:.2f} [s]')
-        print(f'steps to solve: {step}')
+            step += 1
+        print(f"time to solve: {time.time()-t0:.2f} [s]")
+        print(f"steps to solve: {step}")
 
 
 def update_candidates_in_nine(cells):
-    assert len(cells)==9
-    filled = set( cell.val for cell in cells if cell.val!=0 )
+    assert len(cells) == 9
+    filled = set(cell.val for cell in cells if cell.val != 0)
     for cell in cells:
-        if cell.val==0:
-            [ cell.remove_from_candidates(f) for f in filled ]
+        if cell.val == 0:
+            [cell.remove_from_candidates(f) for f in filled]
+
 
 def find_unique_candidates(cells):
-    assert len(cells)==9
+    assert len(cells) == 9
     unique_candidates = []
     for cell in cells:
         candidates = cell.candidates
-        union_other_candidates = set(sum([ list(other.candidates) for other in cells if other.id!=cell.id ],[]))
-        unique_candidate = [ candidate for candidate in candidates if candidate not in union_other_candidates ]
-        if len(unique_candidate)==1:
-            unique_candidates.append({'index':cell.id, 'unique':unique_candidate[0]})
-        elif len(unique_candidate)>1:
-            print('Error!!')
+        union_other_candidates = set(
+            sum([list(other.candidates) for other in cells if other.id != cell.id], [])
+        )
+        unique_candidate = [
+            candidate
+            for candidate in candidates
+            if candidate not in union_other_candidates
+        ]
+        if len(unique_candidate) == 1:
+            unique_candidates.append({"index": cell.id, "unique": unique_candidate[0]})
+        elif len(unique_candidate) > 1:
+            print("Error!!")
             return []
     return unique_candidates
 
+
 def find_pairing_candidates(cells):
-    assert len(cells)==9
+    assert len(cells) == 9
     unique_pairings = []
     for cell in cells:
         candidates = cell.candidates
-        if len(candidates)>1:
-            same_others = [ other.id for other in cells if other.id!=cell.id and candidates==other.candidates ]
-            if len(same_others)==len(candidates)-1:
-                unique_pairings.append({'indices':set([cell.id]+same_others), 'candidates':candidates})
+        if len(candidates) > 1:
+            same_others = [
+                other.id
+                for other in cells
+                if other.id != cell.id and candidates == other.candidates
+            ]
+            if len(same_others) == len(candidates) - 1:
+                unique_pairings.append(
+                    {"indices": set([cell.id] + same_others), "candidates": candidates}
+                )
     return unique_pairings
 
-def exclusive_pairings(cells,pairing_list):
-    assert len(cells)==9
+
+def exclusive_pairings(cells, pairing_list):
+    assert len(cells) == 9
     for pairing in pairing_list:
         for cell in cells:
-            if cell.id not in list(pairing['indices']):
-                for val in pairing['candidates']:
+            if cell.id not in list(pairing["indices"]):
+                for val in pairing["candidates"]:
                     cell.remove_from_candidates(val)
